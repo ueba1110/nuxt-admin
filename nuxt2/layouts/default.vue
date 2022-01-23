6b594c15124b0d04698fb09e2155f3b5ea33be6b
ueba1110 <koji.ueba.1110@gmail.com>
2022-01-24 01:14:06 +0900

    不要なコード整理

--- nuxt2/layouts/default.vue
+++ nuxt2/layouts/default.vue
@@ -76,11 +76,6 @@ export default {
           title: 'Welcome',
           to: '/',
         },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
       ],
       miniVariant: false,
       right: true,
